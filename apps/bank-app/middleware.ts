f6211505b73726d8b6bf8a8b6b3a19f0e142029b
Avinash Kumar Singh <abhinashsinghwk2@gmail.com>
2025-03-08 19:36:58 +0530

    add middleware.ts file to protect routes from unauthenticated users

---
+++ apps/bank-app/middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(request: NextRequest) {
+  const authRoutes = ["/login", "/register", "/forgotpassword"];
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  if (authRoutes.includes(request.nextUrl.pathname) && token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  if (!authRoutes.includes(request.nextUrl.pathname) && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};

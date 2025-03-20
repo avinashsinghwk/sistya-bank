14df86d9e1c11a59f20872d6e49ed397444b9583
Avinash Kumar Singh <abhinashsinghwk2@gmail.com>
2025-03-20 12:45:59 +0530

    remove import files that are not used

--- apps/bank-app/components/custom/DropMenu.tsx
+++ apps/bank-app/components/custom/DropMenu.tsx
@@ -3,20 +3,13 @@ import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuPortal,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { UserAvatar } from "./UserAvatar";
 import { signOut } from "next-auth/react";
-import { CustomButton } from "./CustomButton";
 import { MyLink } from "./MyLink";
 
 export function DropMenu({ username }: { username: string }) {
